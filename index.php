6532e9dda77ae03b99f24005fbc3390ed029d449
zn1k4l3o <leo.znika@tvz.hr>
2024-05-21 23:03:41 +0200

    header

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Monde</title>
+    <link rel="stylesheet" href="style.scss">
+</head>
+<body>
+    <header>
+        <img src="images/leMondeLogo.jpg" alt="Le Monde">
+    </header>
+</body>
+</html>
