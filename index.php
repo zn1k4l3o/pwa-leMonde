699dfb4074d879f99b61d29d6a5cddef8793d2d8
zn1k4l3o <leo.znika@tvz.hr>
2024-05-21 23:59:32 +0200

    feat: index.php and article.php

--- index.php
+++ index.php
@@ -5,10 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le Monde</title>
     <link rel="stylesheet" href="style.scss">
+    <link rel="stylesheet" href="globals.scss">
 </head>
 <body>
     <header>
-        <img src="images/leMondeLogo.jpg" alt="Le Monde">
+        <img src="images/leMondeLogo.jpg" alt="Le Monde" title="Le Monde">
     </header>
+    <nav>
+        <a href="index.php">HOME</a>
+        <a href="#politique">POLITIQUE</a>
+        <a href="#sport">SPORT</a>
+        <a href="#administracija">ADMINISTRACIJA</a>
+    </nav>
+    <section>
+        <hr/>
+        <h2 id="politique">Politique</h2>
+    </section>
+
+    <section>
+        <hr/>
+        <h2 id="sport">Sport</h2>
+    </section>
+
+    <section>
+        <hr/>
+        <h2 id="administracija">Administracija</h2>
+    </section>
 </body>
 </html>
---
+++ pages/article.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Monde</title>
+    <link rel="stylesheet" href="globals.scss">
+    <link rel="stylesheet" href="style.scss">
+</head>
+<body>
+    <header>
+        <img src="images/leMondeLogo.jpg" alt="Le Monde" title="Le Monde">
+    </header>
+    <nav>
+        <a href="index.php">HOME</a>
+        <a href="index.php#politique">POLITIQUE</a>
+        <a href="index.php#sport">SPORT</a>
+        <a href="index.php#administracija">ADMINISTRACIJA</a>
+    </nav>
+</body>
+</html>
